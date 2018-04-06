8a6c78bd40849c5578624c515a43e13141fb8eb4
Arun Babu N <ab@ameyem.com>
2018-04-06 10:54:03 +0530

    assign_task unique changed1

--- database/migrations/tutapp/2018_04_05_162322_chapterstatuses.php
+++ database/migrations/tutapp/2018_04_05_162322_chapterstatuses.php
@@ -15,6 +15,7 @@ class Chapterstatuses extends Migration
     {
         Schema::create('chapterstatuses', function (Blueprint $table) {
             $table->increments('id')->unsigned();
+            
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')
             ->onDelete('cascade');
