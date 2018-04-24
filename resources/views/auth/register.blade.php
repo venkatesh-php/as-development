3cf6aa29da9b6112c3790b75935ceeb3e37f3ecd
venkatesh-php <venkatesh.cse.tec@gmail.com>
2018-04-24 11:27:59 +0530

    navbar transparent

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,44 +2,58 @@
 
 
 @section('content')
-
+<style>
+ .navbar .transparent .navbar-inner {
+    border-width: 0px;
+    -webkit-box-shadow: 0px 0px;
+    box-shadow: 0px 0px;
+    background-color: rgba(0,0,0,0.0);
+    background-image: -webkit-gradient(linear, 50.00% 0.00%, 50.00% 100.00%, color-stop( 0% , rgba(0,0,0,0.00)),color-stop( 100% , rgba(0,0,0,0.00)));
+    background-image: -webkit-linear-gradient(270deg,rgba(0,0,0,0.00) 0%,rgba(0,0,0,0.00) 100%);
+    background-image: linear-gradient(180deg,rgba(0,0,0,0.00) 0%,rgba(0,0,0,0.00) 100%);
+}
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
  <div class="container-fluid">
-        <nav class="navbar navbar-default">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>                        
-                    </button>
-                    
-                    <a class="navbar-brand" href="{{ url('/') }}">
-
-                        <b style="color:white">{{ config('app.name', 'Laravel') }}</b>
-
-                    </a>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav">
+        <nav class="navbar transparent">
+            <div class="navbar-inner">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar">---</span>
+                            <span class="icon-bar">---</span>
+                            <span class="icon-bar">---</span>                        
+                        </button>
                         
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
+                        <a class="navbar-brand" href="{{ url('/') }}">
 
-                        @if (Route::has('login'))
-               
-                            @if (Auth::check())
-                                <li><a href="{{ url('/home') }}"><b style="color:white">Home</b></a></li>
-                                <!-- <li><a href="{{ url('/logout') }}"><b>logout</b></a></li> -->
-                            @else          
-                                <li><a href="{{ url('/') }}"><span style="color:white" class="glyphicon glyphicon-log-in"></span><b style="color:white"> Login</b></a></li>
-                                <!-- <li><a href="{{ url('/register') }}"><span style="color:white" class="glyphicon glyphicon-user"></span><b style="color:white">Registration</b></a></li> -->
+                            <b style="color:white">{{ config('app.name', 'Laravel') }}</b>
+
+                        </a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+
+                            @if (Route::has('login'))
+                
+                                @if (Auth::check())
+                                    <li><a href="{{ url('/home') }}"><b style="color:white">Home</b></a></li>
+                                    <!-- <li><a href="{{ url('/logout') }}"><b>logout</b></a></li> -->
+                                @else          
+                                    <li><a href="{{ url('/') }}"><span style="color:white" class="glyphicon glyphicon-log-in"></span><b style="color:white"> Login</b></a></li>
+                                    <!-- <li><a href="{{ url('/register') }}"><span style="color:white" class="glyphicon glyphicon-user"></span><b style="color:white">Registration</b></a></li> -->
+                                @endif
+                        
                             @endif
-                       
-                        @endif
-                   
-                    </ul>
+                    
+                        </ul>
+                    </div>
                 </div>
             </div>
         </nav>
@@ -81,7 +95,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+            <div class="panel panel-primary">
                 <div class="panel-heading"><center><h2>Ameyem Skills Registration</h2></center></div>
                 <div class="panel-body">
                 {{--  @if(isset($validator))
@@ -339,5 +353,6 @@
         </div>
     </div>
 </div>
+<br><br><br><br><br><br>
 @include("partials.footer")
 @endsection
