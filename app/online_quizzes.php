884fc3ac49ce4c544956fd7b19ad9995b06a3a3b
venkatesh-php <venkatesh.cse.tec@gmail.com>
2018-07-10 21:00:12 +0530

    quiz publish is on/off modified in project

--- app/online_quizzes.php
+++ app/online_quizzes.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class online_quizzes extends Model
 {
-    protected $fillable = ['subject_name','quiz_name','user_id','created_at','updated_at'];
+    protected $fillable = ['subject_name','quiz_name','user_id','publish_status','created_at','updated_at'];
 }
